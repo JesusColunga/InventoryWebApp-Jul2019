6e30dbff53257540777ca15e489aa73cfed61c5a
Jesus Colunga <jesus.colunga@proapi.com.mx>
2019-08-05 21:09:04 -0500

    Test distribution of server and client files for heroku

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -15,7 +15,7 @@ module.exports = function(app) {
   // index route loads view.html
   app.get("/", function(req, res) {
     //res.sendFile(path.join(__dirname, "../public/blog.html"));
-    res.sendFile(path.join(__dirname, "../client/index.html"));
+    res.sendFile(path.join(__dirname, "../client/build"));
   });
 
   app.get("/cms", function(req, res) {
--- server.js
+++ server.js
@@ -13,8 +13,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+app.use(express.static("client/build"));
 require("./routes/api-routes.js")(app);
-require("./routes/html-routes.js")(app);
+//require("./routes/html-routes.js")(app);
 
 // =============================================================
 db.sequelize.sync({ force: false }).then(function() {       // force: true (drops db)
