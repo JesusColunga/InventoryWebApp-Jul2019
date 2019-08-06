3b997534661e45d19b6818d93aa0f22f8861e758
Jesus Colunga <jesus.colunga@proapi.com.mx>
2019-08-05 20:28:38 -0500

    client compiled

--- server/routes/html-routes.js
+++ server/routes/html-routes.js
@@ -14,7 +14,8 @@ module.exports = function(app) {
 
   // index route loads view.html
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
+    //res.sendFile(path.join(__dirname, "../public/blog.html"));
+    res.sendFile(path.join(__dirname, "../client/index.html"));
   });
 
   app.get("/cms", function(req, res) {
